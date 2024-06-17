b2b86189a4d8f862be4c8e2b05b3d02985cb7309
basil <20985102-alemontn@users.noreply.gitlab.com>
2024-06-17 14:22:54 +0100

    add env (external commands) to froyo, change build to froyo source

--- pkg/make-deb.sh
+++ pkg/make-deb.sh
@@ -1,40 +1,32 @@
 #!/usr/bin/env bash
-
+# /usr/bin/froyo src/make-deb.fy -omake-deb.sh -ywerror -ycomments -ynoticeFroyo
 # exit on errors
 set -e
 # show commands as they are being executed
 set -x
-
 # path of git repo
-repo=$(realpath -L ..)
-
-mkdir -p deb/build
+declare repo=$(env realpath -L ..)
+mkdir -p deb/build 
 cd deb/build
-
 # make build structures
-mkdir -p \
-      DEBIAN \
-      usr/bin \
-      usr/lib/froyo/ext
-
+declare -a structs=('DEBIAN' 'usr/bin' 'usr/lib/froyo/ext')
+mkdir -p "${structs[@]}" 
 # copy with permissions
-install -m755 "$repo"/froyo usr/bin/froyo
-
-if [ -n "$GH_WORKFLOW" ] && [ $GH_WORKFLOW -eq 1 ]
-then
+install -m755 "$repo"/froyo usr/bin/froyo 
+if [[ ${#GH_WORKFLOW} -ne 0 && $GH_WORKFLOW -eq 1 ]]
+then {
   # install to system for building
-  sudo mkdir -p /usr/lib/froyo
-  sudo install -m755 "$repo"/froyo /usr/bin/froyo
-fi
-
-# compile each
-for ext in "$repo"/ext/src/*.fy
-do
-  out="${ext##*'/'}" out="${out%'.fy'}".sh
-
-  froyo -o"usr/lib/froyo/ext/$out" -ymodule "$ext"
-done
-
-cat <"$repo"/pkg/DEBIAN >DEBIAN/control
-
-dpkg-deb --root-owner-group --build "$PWD"
+  sudo mkdir -p /usr/lib/froyo 
+  sudo install -m755 "$repo"/froyo /usr/bin/froyo 
+}
+  fi
+  # compile each
+  for ext in "$repo"/ext/src/*.fy
+  do {
+    declare out=${ext##*'/'}
+    out=${out%'.fy'}
+    froyo -o"usr/lib/froyo/ext/$out" -ymodule "$ext" 
+  }
+    done
+    echo "$(<$repo/pkg/DEBIAN)" >DEBIAN/control
+    dpkg-deb --root-owner-group --build "$PWD" 
