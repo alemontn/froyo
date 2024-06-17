0c3054b3f3fd2c90cef02b87e701fce3018a79f0
basil <20985102-alemontn@users.noreply.gitlab.com>
2024-06-17 09:56:30 +0100

    don't change all semicolons to newlines causing corruption

--- pkg/make-deb.sh
+++ pkg/make-deb.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
+# exit on errors
+set -e
+
+# path of git repo
 repo=$(realpath -L ..)
 
 mkdir -p deb/build
 cd deb/build
 
+# make build structures
 mkdir -p \
       DEBIAN \
       usr/bin \
       usr/lib/froyo/ext
 
+# copy with permissions
 install -m755 "$repo"/froyo usr/bin/froyo
 
+# install to system for building
 sudo mkdir -p /usr/lib/froyo
 sudo install -m755 "$repo"/froyo /usr/bin/froyo
 
+# compile each
 for ext in "$repo"/ext/src/*.fy
 do
   out="${ext##*'/'}" out="${out%'.fy'}".sh
